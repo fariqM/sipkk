d0db52c6e351ff1abe952bcd2684f49cb626ed28
Spyder-code <luaysyauqillah@gmail.com>
2022-03-25 04:59:34 +0700

    cc

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="app-footer p-t-10 text-white">
     <div class="container-fluid">
         <p class="text-center small">
-            <a href="https://github.com/fariqM" style="color: azure">
-                DreamyWaze Production &copy; 2019
+            <a href="#" style="color: azure">
+                Copyright &copy; 2022
             </a>
         </p>
     </div>
